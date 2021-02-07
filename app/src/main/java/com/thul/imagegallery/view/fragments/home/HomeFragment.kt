056ef433d6b497174472591480b71161b2cfab2a
unknown geeko <unknown.geeko@gmail.com>
2021-02-07 18:46:51 +0530

    main - fragment added to activity

---
+++ app/src/main/java/com/thul/imagegallery/view/fragments/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.thul.imagegallery.view.fragments.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.thul.imagegallery.R
+
+import dagger.hilt.android.AndroidEntryPoint
+
+
+//@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.home_fragment) {
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+  }
+}
