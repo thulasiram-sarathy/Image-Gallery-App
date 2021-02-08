f79852e8eebbd8b8993721e74fc79ebd0a2f5763
unknown geeko <unknown.geeko@gmail.com>
2021-02-09 00:51:03 +0530

    main - custom imageview created and added to home image grid layout

---
+++ app/src/main/java/com/thul/imagegallery/utils/GridImageView.kt
@@ -0,0 +1,29 @@
+package com.thul.imagegallery.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class GridImageView : ImageView {
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+    }
+
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes) {
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(measuredWidth, measuredWidth)
+    }
+}
