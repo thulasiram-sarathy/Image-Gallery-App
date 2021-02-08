1151a681e341c8ed767de841d3fdd83136e01a5b
unknown geeko <unknown.geeko@gmail.com>
2021-02-09 01:04:03 +0530

    main - home view image adapter added

---
+++ app/src/main/java/com/thul/imagegallery/view/adapter/home/ImageGridAdapter.kt
@@ -0,0 +1,68 @@
+package com.thul.imagegallery.view.adapter.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.thul.imagegallery.R
+import com.thul.imagegallery.model.ImageData
+import com.thul.imagegallery.utils.GridImageView
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class ImageGridAdapter(imageItems: List<ImageData>?) : RecyclerView.Adapter<ImageGridAdapter.renewalViewHolder>() {
+
+  private var list = ArrayList<ImageData>()
+
+  val imagesList = ArrayList(imageItems)
+
+  private lateinit var context: Context
+
+  init {
+    this.list = imagesList
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): renewalViewHolder {
+    val itemView = LayoutInflater.from(parent.context).inflate(
+      R.layout.image_grid_item,
+      parent,
+      false
+    )
+    context = itemView.context
+    return renewalViewHolder(itemView)
+  }
+
+  override fun getItemCount(): Int {
+    return list.size
+  }
+
+  override fun onBindViewHolder(holder: renewalViewHolder, position: Int) {
+    val data = list[position]
+    loadPhoto(holder.gridImage,data.url)
+  }
+
+  fun renewalNotify(cardItems: List<ImageData>) {
+    val initPosition = list.size
+    list.clear()
+    list.addAll(cardItems)
+    notifyItemRangeInserted(initPosition, list.size)
+  }
+
+  class renewalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var gridImage = itemView.findViewById<GridImageView>(R.id.gridImage)!!
+
+  }
+
+  private fun loadPhoto(imageView: GridImageView, url: String?) {
+    Picasso.get()
+      .load(url)
+      .fit()
+      .centerCrop()
+      .placeholder(R.drawable.grid_placeholder)
+      .into(imageView)
+  }
+}
