ed83e11072a7b58cc77ddda3a154090ac208324d
unknown geeko <unknown.geeko@gmail.com>
2021-02-09 02:23:58 +0530

    main - image detail fragment and layout added

---
+++ app/src/main/java/com/thul/imagegallery/view/fragments/detail/DetailFragment.kt
@@ -0,0 +1,21 @@
+package com.thul.imagegallery.view.fragments.detail
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.thul.imagegallery.R
+import com.thul.imagegallery.view.adapter.home.ImageGridAdapter
+import com.thul.imagegallery.viewmodel.home.HomeViewModel
+
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.home_fragment.*
+
+
+@AndroidEntryPoint
+class DetailFragment : Fragment(R.layout.home_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+}
